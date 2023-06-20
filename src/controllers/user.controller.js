2ab881d817854c282e2ed8f8edb4dd8ccc86c239
Phong <vanphong02062002@gmail.com>
2023-06-20 09:27:45 +0700

    add controller register + login, fixed format models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,7 +10,7 @@ const register = async (req, res) => {
       return res.status(400).send("Username or password is not valid");
     }
 
-    if (data.password.length <= 6 || data.username.length <= 3) {
+    if (data.password.length < 6 || data.username.length < 3) {
       return res.status(400).send("Password or username is too short");
     }
 
@@ -25,7 +25,7 @@ const register = async (req, res) => {
       password: hashedPassword,
     });
 
-    await newUser.save();
+    await userService.create(newUser);
     return res.status(200).send("User was created successfully");
   } catch (err) {
     console.error(err);
@@ -33,7 +33,19 @@ const register = async (req, res) => {
   }
 };
 
-const login = (req, res) => {};
+const login = async (req, res) => {
+  try {
+    const data = req.body;
+    const user = await User.findOne({ username: data.username });
+    if (!user) return res.status(404).send("User not found");
+    const passwordIsValid = bcrypt.compareSync(data.password, user.password);
+    if (!passwordIsValid) return res.status(404).send("Password is not valid");
+    return res.status(200).send(user);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = {
   register,
