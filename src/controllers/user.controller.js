f1ccaa7f2bfe00353309915b6d296ef3fc4a0696
Phong <vanphong02062002@gmail.com>
2023-06-27 23:55:55 +0700

    add reftoken

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -57,7 +57,7 @@ const logout = async (req, res) => {
       console.error(error);
       res.status(500).json({ message: "Internal server error." });
     });
-};
+}
 
 const getListUsers = async (req, res) => {
   try {
